92dc5af553652fd600b62593d695f8b2b99a5139
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-06 19:16:17 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,9 @@
+Redmine::Plugin.register :redmine_uhoh do
+  name 'Redmine Uhoh plugin'
+  description 'This plugin keeps track of your exceptions inside Redmine'
+  author 'Jean-Baptiste BARTH'
+  author_url 'mailto:jeanbaptiste.barth@gmail.com'
+  version '0.0.1'
+  url 'https://github.com/jbbarth/redmine_uhoh'
+  requires_redmine :version_or_higher => '2.0.0'
+end
