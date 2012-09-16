5335018bc2bc563c5178802ae97527d86b8106df
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-16 23:32:23 +0200

    Typo in notifications subscriber

--- lib/redmine_uhoh/subscriber.rb
+++ lib/redmine_uhoh/subscriber.rb
@@ -1,4 +1,4 @@
-ActiveSupport::Notification.subscribe "process_action.action_controller" do |payload|
+ActiveSupport::Notifications.subscribe "process_action.action_controller" do |payload|
   if payload[:exception]
     name, message = *payload[:exception]
     Failure.create!(:name => name, :message => message)
