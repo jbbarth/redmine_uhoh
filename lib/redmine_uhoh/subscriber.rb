caa82bc9446986191851e3ec8c54de8e2a2d5a39
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-16 23:59:00 +0200

    Fixed bad params in subcriber block

--- lib/redmine_uhoh/subscriber.rb
+++ lib/redmine_uhoh/subscriber.rb
@@ -1,4 +1,4 @@
-ActiveSupport::Notifications.subscribe "process_action.action_controller" do |payload|
+ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
   if payload[:exception]
     name, message = *payload[:exception]
     Failure.create!(:name => name, :message => message)
