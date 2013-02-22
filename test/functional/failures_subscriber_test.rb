c01fa06a879cae5df5a5e27eca92770f1558397d
jbbarth <jeanbaptiste.barth@gmail.com>
2013-02-22 08:38:32 +0100

    Working functional tests to ensure subscriber works

--- test/functional/failures_subscriber_test.rb
+++ test/functional/failures_subscriber_test.rb
@@ -1,10 +1,6 @@
 require File.expand_path('../../test_helper', __FILE__)
-require 'active_support/log_subscriber/test_helper'
-require 'action_controller/log_subscriber'
 
 class FailuresSubscriberTest < ActionController::TestCase
-  include ActiveSupport::LogSubscriber::TestHelper
-
   fixtures :projects, :trackers, :issue_statuses, :issues,
            :enumerations, :users, :issue_categories,
            :projects_trackers,
@@ -16,23 +12,20 @@ class FailuresSubscriberTest < ActionController::TestCase
            :workflows
 
   def setup
-    super #triggers ActiveSupport::LogSubscriber::TestHelper#setup
     @controller = NewsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     User.current = nil
     @request.session[:user_id] = 2 #jsmith
-    ActionController::LogSubscriber.attach_to(:action_controller)
   end
 
   def test_insert_a_failure
     News.stubs(:visible).raises(Exception.new("Bad robot"))
-    assert_raises Exception do
-      get :index
+    assert_difference 'Failure.count' do
+      assert_raises Exception do
+        get :index
+      end
     end
-    wait
-    #doesn't work.. Failure is not inserted
-    #TODO: find why!
     assert Failure.last.message.match /Bad robot/
   end
 end
