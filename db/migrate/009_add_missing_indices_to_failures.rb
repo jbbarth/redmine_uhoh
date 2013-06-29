bb0c2a8f1d2afaad981bf0e9efee97f0563b09ff
jbbarth <jeanbaptiste.barth@gmail.com>
2013-06-29 23:40:31 +0200

    Add missing indices to failures table

---
+++ db/migrate/009_add_missing_indices_to_failures.rb
@@ -0,0 +1,9 @@
+class AddMissingIndicesToFailures < ActiveRecord::Migration
+  def self.up
+    add_index :failures, :acknowledged
+  end
+
+  def self.down
+    remove_index :failures, :acknowledged
+  end
+end
