db2f91eb9869c53f96a2028b5fde0c003140bd10
Jaap de Haan <jaap.dehaan@alpine.de>
2020-01-29 15:11:42 +0100

    chore: plugin info update

--- init.rb
+++ init.rb
@@ -7,17 +7,18 @@ end
 
 Redmine::Plugin.register :redmine_uhoh do
   name 'Redmine Uhoh plugin'
-  description 'This plugin keeps track of your exceptions inside Redmine'
-  author 'Jean-Baptiste BARTH'
+  description 'Keep track of raised exceptions within Redmine'
+  author 'Jean-Baptiste BARTH (orig)'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  version '0.0.1'
-  url 'https://github.com/jbbarth/redmine_uhoh'
-  requires_redmine :version_or_higher => '2.0.0'
-  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
+  version '1.0.0'
+  url 'https://github.com/tools-aoeur/redmine_uhoh'
+  author_url 'https://github.com/tools-aoeur'
+  requires_redmine version_or_higher: '2.0.0'
+  requires_redmine_plugin :redmine_base_rspec, version_or_higher: '2.0.0' if Rails.env.test?
 end
 
 Redmine::MenuManager.map :admin_menu do |menu|
-  menu.push :failures, {:controller => :failures},
-            :caption => :label_failure_plural,
-            :html => {:class => 'icon'}
+  menu.push :failures, {controller: :failures},
+            caption: :label_failure_plural,
+            html: {class: 'icon'}
 end
